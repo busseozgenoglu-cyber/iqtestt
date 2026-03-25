f83cd68c070d2bd9a4f25a55ce6eb37ee4f9448d
Aleyna <alen@Aleyna-MacBook-Pro.local>
2026-03-25 13:57:52 +0300

    fix: railway host allowed

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true
-  },
   preview: {
     port: process.env.PORT || 3000,
-    host: true
+    host: true,
+    allowedHosts: 'all'
   }
 })
